0a6cf6bc0ac690617b396fb3d184fc75ab8570a0
KacperOdzimek <kacper-odzimek@wp.pl>
2024-01-06 18:27:25 +0100

    final framebuffer update

--- source/objects/framebuffer.h
+++ source/objects/framebuffer.h
@@ -1,47 +1,43 @@
 #pragma once
+#include <map>
+#include <cstdint>
 
 namespace graphics_abstraction
 {
-	
 	struct framebuffer : public object
 	{
 	protected:
-		texture* color_buffer;
+		std::map<uint8_t, texture*> color_buffers;
 		texture* depth_stencil_buffer;
 
 		virtual void bind(internal::pipeline& pipeline) final
 		{
 			pipeline.framebuffer = this;
 		}
-	
-		virtual void set_color_buffer_impl() = 0;
+
+		virtual void set_color_buffer_impl(uint8_t number) = 0;
 		virtual void set_depth_stencil_buffer_impl() = 0;
 
 	public:
-<<<<<<< Updated upstream
-		void set_color_buffer(texture* _color_buffer)
-=======
-		virtual void set_color_buffer(uint8_t number, texture* color_buffer) final
->>>>>>> Stashed changes
+		virtual void clear_color_buffers(float r, float g, float b, float a) = 0;
+		virtual void clear_depth_buffer() = 0;
+		virtual void clear_stencil_buffer() = 0;
+
+		void set_color_buffer(uint8_t number, texture* color_buffer)
 		{
-			color_buffer = _color_buffer;
-			set_color_buffer_impl();
+			color_buffers.insert({ number, color_buffer });
+			set_color_buffer_impl(number);
 		}
-		virtual void set_depth_stencil_buffer(texture* _depth_stencil_buffer) final
+		void set_depth_stencil_buffer(texture* _depth_stencil_buffer)
 		{
 			depth_stencil_buffer = _depth_stencil_buffer;
 			set_depth_stencil_buffer_impl();
 		}
-<<<<<<< Updated upstream
-		virtual texture* get_color_buffer() final
-=======
-		virtual void clear_color_buffers(float r, float g, float b, float a) = 0;
-		virtual void clear_depth_buffer() = 0;
-		virtual void clear_stencil_buffer() = 0;
 		virtual texture* get_color_buffer(uint8_t number) final
->>>>>>> Stashed changes
 		{
-			return color_buffer;
+			if (color_buffers.find(number) == color_buffers.end())
+				return nullptr;
+			return color_buffers.at(number);
 		}
 		virtual texture* get_depth_stencil_buffer() final
 		{
@@ -51,7 +47,7 @@ namespace graphics_abstraction
 
 	struct framebuffer_builder : public builder
 	{
-		texture* color_buffer = nullptr;
+		std::map<uint8_t, texture*> color_buffers;
 		texture* depth_stencil_buffer = nullptr;
 	protected:
 		virtual object* build_abs(api*& api) final
